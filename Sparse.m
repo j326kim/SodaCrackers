bc9f34a882e01c541a2cd187be96423af72cdc1f
Daniel <jeremykim000@gmail.com>
2013-11-13 21:27:17 -0500

    Deleted Sparse2 and merged it with sparse
    Added a flag to sparse so that when the program is in the middle of
    testing the bow, it will not sparse the F,U,Ud, and Udd vectors.

--- Sparse.m
+++ Sparse.m
@@ -1,18 +1,28 @@
-function [G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F] = Sparse(G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F,Nodes)
+function [G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F] = Sparse(G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F,Nodes,flag)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    
-    for i=1:length(Nodes)
-        G_K( Nodes(i),:)=[]; %remove row
-        G_K(:,Nodes(i))=[]; %remove column
-        G_C( Nodes(i),:)=[]; %remove row
-        G_C(:,Nodes(i))=[]; %remove column
-        G_M( Nodes(i),:)=[]; %remove row
-        G_M(:,Nodes(i))=[]; %remove column
-        G_F(Nodes(i),1)=[];
-        G_U(Nodes(i),1)=[];
-        G_Ud(Nodes(i),1)=[];
-        G_Udd(Nodes(i),1)=[];
+    if flag==1
+        for i=1:length(Nodes)
+            G_K( Nodes(i),:)=[]; %remove row
+            G_K(:,Nodes(i))=[]; %remove column
+            G_C( Nodes(i),:)=[]; %remove row
+            G_C(:,Nodes(i))=[]; %remove column
+            G_M( Nodes(i),:)=[]; %remove row
+            G_M(:,Nodes(i))=[]; %remove column
+            G_F(Nodes(i),1)=[];
+            G_U(Nodes(i),1)=[];
+            G_Ud(Nodes(i),1)=[];
+            G_Udd(Nodes(i),1)=[];
+        end
+    else
+        for i=1:length(Nodes)
+            G_K( Nodes(i),:)=[]; %remove row
+            G_K(:,Nodes(i))=[]; %remove column
+            G_C( Nodes(i),:)=[]; %remove row
+            G_C(:,Nodes(i))=[]; %remove column
+            G_M( Nodes(i),:)=[]; %remove row
+            G_M(:,Nodes(i))=[]; %remove column
+        end
     end
 end
 
