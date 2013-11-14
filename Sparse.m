7946fe584e119f7c6bf2b9fb0db41481ad6a895b
Daniel <jeremykim000@gmail.com>
2013-11-13 21:15:13 -0500

    Put everything in one for loop
    Changed one of the inputs to a vector of rows/columns that need to be
    removed

--- Sparse.m
+++ Sparse.m
@@ -3,30 +3,16 @@ function [G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_F] = Sparse(G_K,G_C,G_M,G_U,G_Ud,G_Udd,G_
 %   Detailed explanation goes here
     
     for i=1:length(Nodes)
-    G_K(:,23) = [];
-    G_K(23,:) = [];
-    G_C(:,23) = [];
-    G_C(23,:) = [];
-    G_M(:,23) = [];
-    G_M(23,:) = [];
-    G_F(23) = [];
-        G_U(23)=[];
-        G_Ud(23)=[];
-        G_Udd(23)=[];
-
-
-
-    for i = 0:2
-        G_K=removerows(G_K,3*centre-i); %remove row
-        G_K(:,3*centre-i)=[]; %remove column
-        G_C=removerows(G_C,3*centre-i);
-        G_C(:,3*centre-i)=[];
-        G_M=removerows(G_M,3*centre-i);
-        G_M(:,3*centre-i)=[];
-        G_F=removerows(G_F,3*centre-i);
-        G_U=removerows(G_U,3*centre-i);
-        G_Ud=removerows(G_Ud,3*centre-i);
-        G_Udd=removerows(G_Udd,3*centre-i);
+        G_K( Nodes(i),:)=[]; %remove row
+        G_K(:,Nodes(i))=[]; %remove column
+        G_C( Nodes(i),:)=[]; %remove row
+        G_C(:,Nodes(i))=[]; %remove column
+        G_M( Nodes(i),:)=[]; %remove row
+        G_M(:,Nodes(i))=[]; %remove column
+        G_F(Nodes(i),1)=[];
+        G_U(Nodes(i),1)=[];
+        G_Ud(Nodes(i),1)=[];
+        G_Udd(Nodes(i),1)=[];
     end
 end
 
